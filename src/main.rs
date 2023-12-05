9f04b9bc7b99c823cc89462a2c3644aa664798ff
Koen Molenaar <koen.molenaar26@gmail.com>
2023-12-05 01:29:48 +0100

    Working on chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 //! main.rs
 
+use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to create database pool");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connection_pool)?.await
 }
